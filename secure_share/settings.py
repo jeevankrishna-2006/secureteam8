9f731566b4623d6947d746fe7ba5ebb441485935
jeevankrishna-2006 <puppalajeevankrishna2006@gmail.com>
2026-02-09 19:23:25 +0530

    fixed otp render env

--- secure_share/settings.py
+++ secure_share/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -126,8 +127,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "puppalajeevankrishna2006@gmail.com"
-EMAIL_HOST_PASSWORD = "wtghewtzdhdwmlmg"
+EMAIL_HOST_USER = os.environ.get("puppalajeevankrishna2006@gmail.com")
+EMAIL_HOST_PASSWORD = os.environ.get("wtghewtzdhdwmlmg")
 FERNET_KEY = "OKvItJ8b18tOmIEWRIL3PdhpW5kzdsWqy8dG2uOCKnY="
 LOGIN_URL = "/login/"
 LOGIN_REDIRECT_URL = "/dashboard/"
